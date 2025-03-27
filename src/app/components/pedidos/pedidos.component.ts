2ac8481ea4da3b83c17e5578ae13434e1f1aa938
Sander Balcazar <sander4.rol@gmail.com>
2025-03-27 00:33:11 -0600

    inicio de vista de pedidos

--- src/app/components/pedidos/pedidos.component.ts
+++ src/app/components/pedidos/pedidos.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Pedidos } from '../../models/pedidos.models';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PedidosService } from '../../services/pedidos.service';
+import { Clientes } from '../../models/clientes.models';
+import { ClientesService } from '../../services/clientes.service';
 
 @Component({
   selector: 'app-pedidos',
@@ -7,5 +13,63 @@ import { Component } from '@angular/core';
   styleUrl: './pedidos.component.css'
 })
 export class PedidosComponent {
+  pedidos: Pedidos [] = [];
+  clientesDisponibles: Clientes[] = []
+  pedidosForm: FormGroup;
+  tituloModal: string = 'agregar pedidos';
+  isEditMode: Boolean = false;
+  selectedProducto: any = null;
 
+  constructor(private modalService: NgbModal,
+    private pedidosService: PedidosService,
+    private formBuilder: FormBuilder,
+    private clientesService: ClientesService 
+  ){
+       this.pedidosForm = this.formBuilder.group({
+      id: [null],
+      cliente: [null, Validators.required],
+      total: [0, [Validators.required, Validators.min(0)]],
+      fechaCreacion: [new Date()],
+      estado: ['PENDIENTE', Validators.required]
+    });
+  }
+
+  ngOnInit(){
+    this.loadPedidos
+  }
+  loadPedidos(){
+    this.pedidosService.getPedidos().subscribe({
+
+      next: data =>{
+        this.pedidos = data;
+        console.log('Pedidos cargados:', this.pedidos); 
+      }
+    });
+  }
+
+  abrirModal(modal: any, pedido?: Pedidos): void {
+    this.isEditMode = !!pedido;
+    this.tituloModal = this.isEditMode ? 'Editar Pedido' : 'Nuevo Pedido';
+    
+    if (pedido) {
+      this.pedidosForm.patchValue(pedido);
+    } else {
+      this.pedidosForm.reset({
+        total: 0,
+        fechaCreacion: new Date(),
+        estado: 'PENDIENTE'
+      });
+    }
+
+    this.modalService.open(modal, { centered: true, size: 'lg' });
+  }
+
+  onSubmit(): void {
+    if (this.pedidosForm.valid) {
+      const pedidoData: Pedidos = this.pedidosForm.value;
+      // Lógica para guardar/actualizar el pedido
+    }
+  }
 }
+
+
--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -8,11 +8,20 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PedidosService {
-  private apiUrl = environment.apiUrl + 'pedidos';
+  private apiUrl = environment.apiUrl + 'pedidos/';
   constructor(private http: HttpClient) { }
 
-    getPedidos(): Observable<Pedidos[]> {
-      return this.http.get<Pedidos[]>(this.apiUrl);
-    }
+      getPedidos(): Observable<Pedidos[]> {
+        return this.http.get<Pedidos[]>(this.apiUrl);
+      }
+      createPedidos(pedidos: Pedidos): Observable<Pedidos> {
+        return this.http.post<Pedidos>(this.apiUrl, pedidos);
+      }
+      updatePedidos(pedidos: Pedidos): Observable<Pedidos> {
+        return this.http.put<Pedidos>(`${this.apiUrl}${pedidos.id}`, pedidos);
+      }
+      deletePedidos(id: number): Observable<Pedidos> {
+        return this.http.delete<Pedidos>(this.apiUrl + id);
+      }
 }
 
